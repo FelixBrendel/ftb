607af164c5f204e5e0116f811ba9c14f3efedef5
Felix Brendel <felix@brendel.io>
2023-04-22 18:22:25 +0200

    lkad

--- tests/build.sh
+++ tests/build.sh
@@ -10,9 +10,9 @@ time clang++ -rdynamic -D_DEBUG -D_PROFILING -fpermissive main.cpp -gdwarf-4 -o 
 # time clang++ -D_DEBUG -D_PROFILING -fpermissive cpu_info.cpp -g -o ./cpu_info --std=c++17 || exit 1
 
 echo ""
-time valgrind --track-origins=yes --leak-check=full --show-leak-kinds=all ./ftb
+# time valgrind --track-origins=yes --leak-check=full --show-leak-kinds=all ./ftb
 # valgrind ./ftb
-# time ./ftb
+time ./ftb || exit 1
 # time ./cpu_info
 
 popd > /dev/null
